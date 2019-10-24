3500c9638d1fcc6510c37f2143f9be8011202b28
Deepak <desharma@ucsc.edu>
2019-10-24 13:37:52 -0700

    Update StorageHelper.cs
    modified file

--- ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Helpers/StorageHelper.cs
@@ -22,7 +22,7 @@ namespace ImageResizeWebApp.Helpers
                 return true;
             }
 
-            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg" };
+            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg", ".csv" };
 
             return formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
         }
